355f2c0e150b5c8124bbe409ad28e1ee4ca190ab
Sergio Orizano <svorizano@gmail.com>
2023-06-01 20:58:02 -0500

    nuevo

--- IA_1.py
+++ IA_1.py
@@ -1,37 +1,32 @@
-pip install streamlit-webrtc
-
-
 import streamlit as st
-import streamlit_webrtc as webrtc
+import cv2
 
 # Configurar la página de la aplicación
-st.title("Proyecto de Lenguaje de Señas")
+st.title("Activar Cámara")
 
-# Función para mostrar la imagen correspondiente a la letra seleccionada
-def mostrar_lenguaje_letra_A():
-    st.image('ruta_a_imagen_A.jpg', caption='Lenguaje de señas - Letra A')
+# Mostrar los textos "UNSA" y "Gabriela Salas"
+st.write("UNSA", "Gabriela Salas")
 
 # Botón para activar la cámara
-activar_camara = st.button("Activar cámara")
+activar_camara = st.button("Activar Cámara")
 
 # Verificar si se ha activado la cámara
 if activar_camara:
-    st.write("Cámara activada")
-    webrtc_ctx = webrtc_streamer(
-        key="example",
-        mode=WebRtcMode.SENDRECV,
-        video_transformer_factory=None,
-        async_transform=True,
-    )
+    # Inicializar la captura de video desde la cámara
+    cap = cv2.VideoCapture(0)
 
-    # Mostrar el video de la cámara
-    if webrtc_ctx.video_receiver:
-        video_frame = webrtc_ctx.video_receiver.get_frame()
-        st.image(video_frame)
+    # Bucle para capturar y mostrar los fotogramas de la cámara
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        # Mostrar el fotograma capturado
+        st.image(frame, channels="BGR")
 
-        # Realizar la detección de la letra "A" en el fotograma actual
-        # Aquí debes agregar tu código de detección utilizando técnicas de procesamiento de imágenes y aprendizaje automático
+        # Verificar si se ha presionado el botón de detener
+        if st.button("Detener"):
+            break
 
-        # Si se detecta la letra "A", mostrar la imagen correspondiente
-        if deteccion_letra_A:
-            mostrar_lenguaje_letra_A()
+    # Liberar los recursos de la cámara
+    cap.release()
