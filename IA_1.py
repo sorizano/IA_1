8e59a5d212d3da80039db294d4c369e842b30378
Sergio Orizano <svorizano@gmail.com>
2023-06-01 20:52:01 -0500

    Primeros pasos

--- IA_1.py
+++ IA_1.py
@@ -1,1 +1,44 @@
+import cv2
+import os
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+# Descargar la imagen de la letra "A" desde Google Drive
+output_dir = './lenguaje'
+os.makedirs(output_dir, exist_ok=True)
+gdd.download_file_from_google_drive(file_id='1byE1Yt4MuyEf5q_GQhVQc7I96LsPfTpa', dest_path=f'{output_dir}/A.jpg', unzip=False)
+
+# Ruta de la imagen de la letra "A" en tu directorio local
+ruta_imagen_A = './lenguaje/A.jpg'
+
+# Cargar la imagen de la letra "A"
+imagen_A = cv2.imread(ruta_imagen_A)
+
+# Función para mostrar la imagen de la letra "A" cuando se detecta
+def mostrar_lenguaje_letra_A():
+    cv2.imshow("Lenguaje de señas - Letra A", imagen_A)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# Captura de video desde la cámara
+video_captura = cv2.VideoCapture(0)  # El número 0 indica que se utilizará la cámara predeterminada
+
+# Bucle para la detección y visualización en tiempo real
+while True:
+    ret, frame = video_captura.read()  # Captura un fotograma de la cámara
+
+    # Realizar la detección de la letra "A" en el fotograma actual
+    # Aquí debes agregar tu código de detección utilizando técnicas de procesamiento de imágenes y aprendizaje automático
+
+    # Si se detecta la letra "A", mostrar la imagen correspondiente
+    if deteccion_letra_A:
+        mostrar_lenguaje_letra_A()
+
+    # Mostrar el fotograma en una ventana
+    cv2.imshow("Cámara", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Presiona 'q' para salir del bucle
+        break
+
+video_captura.release()  # Libera los recursos de la cámara
+cv2.destroyAllWindows()
 
